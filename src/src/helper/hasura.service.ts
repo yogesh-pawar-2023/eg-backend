3982c6ce549db5530c45ecf0fc55ab78f6b7c7b4
Sagar Takle <sagar_t@techjoomla.com>
2023-04-27 08:47:04 +0000

    form submit bugs + add email_id

--- src/src/helper/hasura.service.ts
+++ src/src/helper/hasura.service.ts
@@ -41,4 +41,227 @@ export class HasuraService {
         .pipe(map((res) => res.data)),
     );
   }
+
+  queryMulti(tableName: String, items: any, onlyFields: any, fields: any = []) {
+    let returnkeys = [];
+    const getObjStr = (item: Object, type: String = '') => {
+      let str = '[';
+      items.forEach((item, pindex) => {
+        const keys = Object.keys(item);
+        str += '{';
+        keys.forEach((e, index) => {
+          if (!returnkeys.includes(e)) {
+            returnkeys = [...returnkeys, e];
+          }
+          if (onlyFields.length < 1 || onlyFields.includes(e)) {
+            if (type === 'obj') {
+              str += `${e}:"${item[e]}"${keys.length > index + 1 ? ',' : ''}`;
+            } else {
+              str += `$${e}:String${keys.length > index + 1 ? ',' : ''}`;
+            }
+          }
+        });
+        str += `}${items.length > pindex + 1 ? ',' : ''}`;
+      });
+      return (str += ']');
+    };
+
+    const getParam = (keys: any) => {
+      let str = '';
+      keys.forEach((e: any, index: any) => {
+        str += `${e}${keys.length > index + 1 ? '\n' : ''}`;
+      });
+      return str;
+    };
+
+    return `mutation MyQuery {
+      ${tableName}(objects: ${getObjStr(items, 'obj')}) {
+        returning {${getParam(
+          fields ? fields : onlyFields ? [...onlyFields, 'id'] : returnkeys,
+        )}}
+      }
+    }
+    `;
+  }
+
+  deleteQuery(tName: String, item: any, onlyFields: any = []) {
+    let tableName = `delete_${tName}`;
+    const keys = Object.keys(item);
+
+    const getObjStr = (item: any, type: String = '') => {
+      let str = ``;
+      let strArr = [];
+      keys.forEach((e) => {
+        if (onlyFields.length < 1 || onlyFields.includes(e)) {
+          if (type === 'obj') {
+            strArr = [...strArr, `${e}:{_eq:"${item[e]}"}`];
+          }
+        }
+      });
+      str += strArr.join();
+      return str;
+    };
+
+    return `mutation DeleteQuery {
+      ${tableName}(where: {${getObjStr(item, 'obj')}}) {
+         affected_rows
+      }
+    }
+    `;
+  }
+
+  async delete(tableName: String, item: Object, onlyFields: any = []) {
+    return this.getResponce(
+      await lastValueFrom(
+        this.httpService
+          .post(
+            this.url,
+            {
+              query: this.deleteQuery(tableName, item, onlyFields),
+            },
+            {
+              headers: {
+                'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
+                'Content-Type': 'application/json',
+              },
+            },
+          )
+          .pipe(map((res) => res.data)),
+      ),
+      tableName,
+    );
+  }
+
+  query(
+    tName: String,
+    item: any,
+    onlyFields: any = [],
+    update: boolean = false,
+    fields: any = [],
+  ) {
+    let tableName = `insert_${tName}_one`;
+    if (item?.id && update) {
+      tableName = `update_${tName}`;
+    }
+    const keys = Object.keys(item);
+    const getObjStr = (item: any, type: String = '') => {
+      let str = 'object: {';
+      if (item?.id && update) {
+        str = `where: {id: {_eq: ${item?.id}}}, _set: {`;
+      }
+      let strArr = [];
+      keys.forEach((e, index) => {
+        if (e !== 'id' && (onlyFields.length < 1 || onlyFields.includes(e))) {
+          if (type === 'obj') {
+            strArr = [...strArr, `${e}:"${item[e]}"`];
+          } else {
+            strArr = [...strArr, `${e}:String`];
+          }
+        }
+      });
+      str += strArr.join();
+      str += `}`;
+      return str;
+    };
+
+    const getParam = (keys: any) => {
+      let str = '';
+      keys.forEach((e: any, index: any) => {
+        str += `${e}${keys.length > index + 1 ? '\n' : ''}`;
+      });
+      return str;
+    };
+
+    return `mutation MyQuery {
+      ${tableName}(${getObjStr(item, 'obj')}) {
+        ${
+          !(item?.id && update)
+            ? getParam(
+                fields && fields.length > 0
+                  ? fields
+                  : onlyFields
+                  ? [...onlyFields, 'id']
+                  : keys,
+              )
+            : 'affected_rows'
+        }
+      }
+    }
+    `;
+  }
+
+  async q(
+    tableName: String,
+    item: Object,
+    onlyFields: any = [],
+    update: boolean = false,
+    fields: any = [],
+  ) {
+    return this.getResponce(
+      await lastValueFrom(
+        this.httpService
+          .post(
+            this.url,
+            {
+              query: this.query(tableName, item, onlyFields, update, fields),
+            },
+            {
+              headers: {
+                'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
+                'Content-Type': 'application/json',
+              },
+            },
+          )
+          .pipe(map((res) => res.data)),
+      ),
+      tableName,
+    );
+  }
+
+  async qM(tableName: String, item: any, fields: any, onlyFields: any = []) {
+    return this.getResponce(
+      await lastValueFrom(
+        this.httpService
+          .post(
+            this.url,
+            {
+              query: this.queryMulti(tableName, item, fields, onlyFields),
+            },
+            {
+              headers: {
+                'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
+                'Content-Type': 'application/json',
+              },
+            },
+          )
+          .pipe(map((res) => res.data)),
+      ),
+      tableName,
+    );
+  }
+
+  public getResponce = (
+    { data, errors }: any,
+    tableName: any,
+    response = 'table',
+  ) => {
+    let result = null;
+    if (data) {
+      if (data[`insert_${tableName}_one`]) {
+        result = data[`insert_${tableName}_one`];
+      } else if (data[`update_${tableName}`]) {
+        result = data[`update_${tableName}`];
+      } else if (data[`delete_${tableName}`]) {
+        result = data[`delete_${tableName}`];
+      } else {
+        result = data[tableName];
+      }
+    }
+    result = result ? result : errors ? errors[0] : {};
+    if (response === 'data') {
+      return result;
+    } else {
+      return { [tableName]: result };
+    }
+  };
 }
--- src/src/user.controller.ts
+++ src/src/user.controller.ts
@@ -13,7 +13,6 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { log } from 'console';
 import { lastValueFrom, map } from 'rxjs';
 import { CreateUserDto } from './helper/dto/create-user.dto';
 import { RegisterFacilitatorDto } from './helper/dto/register-facilitator.dto';
@@ -40,6 +39,7 @@ export class UserController {
               qualification_masters {
                 id
                 name
+                type
               }
             }`,
           },
@@ -52,9 +52,18 @@ export class UserController {
         )
         .pipe(map((res) => res.data)),
     );
-    return data;
+    return {
+      statusCode: 200,
+      message: 'Ok.',
+      data: this.hasuraService.getResponce(
+        data,
+        'qualification_masters',
+        'data',
+      ),
+    };
   }
 
+  // users/create API
   @Post('/create')
   @HttpCode(200)
   @UsePipes(ValidationPipe)
@@ -62,238 +71,35 @@ export class UserController {
     return this.userService.create(req);
   }
 
+  @Put('/update/:id')
+  @HttpCode(200)
+  @UsePipes(ValidationPipe)
+  public async update(
+    @Param('id') id: number,
+    @Body() req: Record<string, any>,
+  ) {
+    return this.userService.create({ ...req, id: id }, true);
+  }
+
   // users/list API filter pagination
   @Post('/list')
   public async searchAttendance(@Body() request: Record<string, any>) {
-    const { filters } = request;
-    const page = request.page ? request.page : '1';
-    const limit = request?.limit ? request?.limit : '10';
-
-    let offset = 0;
-    if (page > 1 && limit) {
-      offset = parseInt(limit) * (page - 1);
-    }
-
-    let query = '';
-    if (filters) {
-      Object.keys(filters).forEach((e) => {
-        if (filters[e] && filters[e] != '') {
-          query += `${e}:{_eq:"${filters[e]}"}`;
-        }
-      });
-    }
-    query += `program_faciltators: {id: {_is_null: false}, parent_ip: {_eq: "1"}}`;
-    var data = {
-      query: `query SearchAttendance($limit:Int, $offset:Int) {
-        users_aggregate(where:{${query}}) {
-          aggregate {
-            count
-          }
-        }
-        users(where:{${query}}, limit: $limit, offset: $offset) {
-          first_name
-          id
-          last_name
-          dob
-          aadhar_token
-          address
-          block_id
-          block_village_id
-          created_by
-          district_id
-          email_id
-          gender
-          lat
-          long
-          mobile
-          password
-          state_id
-          updated_by
-          core_faciltator {
-            device_ownership
-            device_type
-            id
-            pan_no
-            user_id
-          }
-          experience {
-            description
-            end_year
-            experience_in_years
-            institution
-            start_year
-            organization
-            role_title
-            user_id
-          }
-          program_faciltators {
-            id
-            status
-            parent_ip
-            avaibility
-            has_social_work_exp
-            id
-            police_verification_done
-            program_id
-            user_id
-            social_background_verified_by_neighbours
-          }
-          qualifications {
-            end_year
-            id
-            institution
-            start_year
-            user_id
-            qualification_master {
-              name
-              id
-            }
-          }
-        }}`,
-      variables: {
-        limit: parseInt(limit),
-        offset: offset,
-      },
-    };
-
-    const response = await lastValueFrom(
-      this.httpService
-        .post(this.url, data, {
-          headers: {
-            'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
-            'Content-Type': 'application/json',
-          },
-        })
-        .pipe(map((res) => res.data)),
-    );
-
-    let result = response?.data?.users;
-
-    let mappedResponse = result;
-    const count = response?.data?.users_aggregate?.aggregate?.count;
-    const totalPages = Math.ceil(count / limit);
-
-    return {
-      statusCode: 200,
-      message: 'Ok.',
-      totalCount: count,
-      data: mappedResponse,
-      limit,
-      currentPage: page,
-      totalPages: `${totalPages}`,
-    };
+    return this.userService.list(request);
   }
 
   // users/list by ID API filter pagination
   @Get('/info/:id')
   public async searchById(@Param('id') id: number) {
-    var data = {
-      query: `query searchById {        
-        users_by_pk(id: ${id}) {
-          first_name
-          id
-          last_name
-          dob
-          aadhar_token
-          address
-          block_id
-          block_village_id
-          created_by
-          district_id
-          email_id
-          gender
-          lat
-          long
-          mobile
-          password
-          state_id
-          updated_by
-          core_faciltator {
-            device_ownership
-            device_type
-            id
-            pan_no
-            user_id
-          }
-          experience {
-            description
-            end_year
-            experience_in_years
-            institution
-            start_year
-            organization
-            role_title
-            user_id
-          }
-          program_faciltators {
-            id
-            status
-            avaibility
-            has_social_work_exp
-            id
-            police_verification_done
-            program_id
-            user_id
-            social_background_verified_by_neighbours
-          }
-          qualifications {
-            end_year
-            id
-            institution
-            start_year
-            user_id
-            qualification_master {
-              name
-              id
-            }
-          }
-        }}`,
-    };
-
-    const response = await lastValueFrom(
-      this.httpService
-        .post(this.url, data, {
-          headers: {
-            'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
-            'Content-Type': 'application/json',
-          },
-        })
-        .pipe(map((res) => res.data)),
-    );
-    const result = response?.data?.users_by_pk;
-    const mappedResponse = result;
-
-    return {
-      statusCode: 200,
-      message: 'Ok.',
-      data: mappedResponse,
-    };
+    return this.userService.userById(id);
   }
+
+  // users/is_user_exist by mobile and adhaar etc filter.
   @Post('/is_user_exist')
   public async isUserExist(@Body() req: Record<string, any>) {
-    // Set User table name
-    const tableName = 'users';
-
-    // Calling hasura common method find all
-    const data_exist = await this.hasuraService.findAll(tableName, req);
-    let response = data_exist.data.users;
-
-    // Check wheather user is exist or not based on response
-    if (response.length > 0) {
-      return {
-        status: 422,
-        message: 'User exist',
-        isUserExist: true,
-      };
-    } else {
-      return {
-        status: 200,
-        message: 'User not exist',
-        isUserExist: false,
-      };
-    }
+    return this.userService.isUserExist(req);
   }
 
+  // users/update_facilitator/:id update facilitator status.
   @Put('update_facilitator/:id')
   public async updateUser(
     @Param('id') id: string,
@@ -302,6 +108,7 @@ export class UserController {
     return this.userService.update(id, req, 'program_faciltators');
   }
 
+  // users/login by username and password.
   @Post('/login')
   login(
     @Query('username') username: string,
@@ -309,12 +116,15 @@ export class UserController {
   ) {
     return this.userService.login(username, password);
   }
+
+  // users/ip_user_info by auth token.
   @Get('/ip_user_info')
   ipUserInfo(@Req() request: Request) {
     return this.userService.ipUserInfo(request);
   }
 
-  @Post('/user_create')
+  // users/register on keycloak and hasura both side.
+  @Post('/register')
   @HttpCode(200)
   @UsePipes(ValidationPipe)
   public async register(
