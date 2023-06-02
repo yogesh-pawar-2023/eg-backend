90f6522174900c15078c870eae5a6c4fcb1aa625
yogpawar1206 <107765181+yogpawar1206@users.noreply.github.com>
2023-06-02 12:58:45 +0530

    ..

--- src/src/events/events.controller.ts
+++ src/src/events/events.controller.ts
@@ -19,38 +19,51 @@ import { EventsService } from './events.service';
 
 @Controller('events')
 export class EventsController {
-  constructor(private readonly eventsService: EventsService) {}
+	constructor(private readonly eventsService: EventsService) {}
 
-  @Post('/create')
-  @UseGuards(new AuthGuard())
-  @UsePipes(ValidationPipe)
-  create(@Body() createEventDto: CreateEventDto, @Req() header: Request, @Res() response: Response) {
-    return this.eventsService.create(createEventDto, header, response);
-  }
+	@Post('/create')
+	@UseGuards(new AuthGuard())
+	@UsePipes(ValidationPipe)
+	create(
+		@Body() createEventDto: CreateEventDto,
+		@Req() header: Request,
+		@Res() response: Response,
+	) {
+		return this.eventsService.create(createEventDto, header, response);
+	}
 
-  @Get('/list')
-  @UseGuards(new AuthGuard())
-  getEventsList(@Body() body: any, @Req() header: Request, @Res() response: Response) {
-    return this.eventsService.getEventsList(body, header, response);
-  }
+	@Get('/list')
+	@UseGuards(new AuthGuard())
+	getEventsList(
+		@Body() body: any,
+		@Req() header: Request,
+		@Res() response: Response,
+	) {
+		return this.eventsService.getEventsList(body, header, response);
+	}
 
-  @Post()
-  findAll(@Body() request: Record<string, any>) {
-    return this.eventsService.findAll(request);
-  }
+	@Post()
+	findAll(@Body() request: Record<string, any>) {
+		return this.eventsService.findAll(request);
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string, @Res() response: Response) {
-    return this.eventsService.findOne(+id,response);
-  }
+	@Get(':id')
+	findOne(@Param('id') id: string, @Res() response: Response) {
+		return this.eventsService.findOne(+id, response);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string,@Req() header: Request, @Body() request: Record<string, any>,@Res() response: Response) {
-    return this.eventsService.update(+id, header,request,response);
-  }
+	@Patch(':id')
+	update(
+		@Param('id') id: string,
+		@Req() header: Request,
+		@Body() request: Record<string, any>,
+		@Res() response: Response,
+	) {
+		return this.eventsService.update(+id, header, request, response);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.eventsService.remove(+id);
-  }
+	@Delete(':id')
+	remove(@Param('id') id: string) {
+		return this.eventsService.remove(+id);
+	}
 }
