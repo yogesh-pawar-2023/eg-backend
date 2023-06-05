4e9c49bbf0ee236d02bbcbba90f589aaac32a4fa
yogesh-pawar-2023 <133862746+yogesh-pawar-2023@users.noreply.github.com>
2023-06-05 20:04:09 +0530

    update existing docs from s3 bucket and document table (#86)
    * delete existing docs from s3 bucket and document table
    
    * file formatted
    
    ---------
    
    Co-authored-by: yogpawar1206 <107765181+yogpawar1206@users.noreply.github.com>

--- src/src/services/s3/s3.service.ts
+++ src/src/services/s3/s3.service.ts
@@ -1,69 +1,87 @@
-import { Injectable } from '@nestjs/common';
 import {
-    S3Client,
-    PutObjectCommand,
-    PutObjectCommandInput,
-    PutObjectCommandOutput,
-    GetObjectCommand
-} from "@aws-sdk/client-s3";
-import { ConfigService } from '@nestjs/config';
+  DeleteObjectCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+  PutObjectCommandInput,
+  PutObjectCommandOutput,
+  S3Client,
+} from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class S3Service {
-    private region: string;
-    private s3: S3Client;
+	private region: string;
+	private s3: S3Client;
 
-    constructor(private configService: ConfigService) {
-        this.region = this.configService.get<string>('S3_REGION')
-        this.s3 = new S3Client({
-            region: this.region,
-            credentials: {
-                secretAccessKey: this.configService.get<string>('SECRET_ACCESS_KEY'),
-                accessKeyId: this.configService.get<string>('ACCESS_KEY_ID')
-            }
-        })
-    }
+	constructor(private configService: ConfigService) {
+		this.region = this.configService.get<string>('S3_REGION');
+		this.s3 = new S3Client({
+			region: this.region,
+			credentials: {
+				secretAccessKey:
+					this.configService.get<string>('SECRET_ACCESS_KEY'),
+				accessKeyId: this.configService.get<string>('ACCESS_KEY_ID'),
+			},
+		});
+	}
 
-    async uploadFile(file: Express.Multer.File, key: string) {
-        console.log("inside upload file")
-        const bucket = this.configService.get<string>('S3_BUCKET')
-        const expiresIn = this.configService.get<number>('EXPIRES_IN')
-        const input: PutObjectCommandInput = {
-            Body: file.buffer,
-            Bucket: bucket,
-            Key: key,
-            ContentType: file.mimetype
-        };
-        console.log("input", input)
-        try {
-            const response: PutObjectCommandOutput = await this.s3.send(
-                new PutObjectCommand(input),
-            );
-            console.log("response", response)
-            if (response.$metadata.httpStatusCode === 200) {
-                const client = this.s3;
-                const command = new GetObjectCommand({ Bucket: bucket, Key: key });
-                return getSignedUrl(client, command, { expiresIn: expiresIn });
-            }
-            throw new Error('File not saved to s3!')
-        } catch (err) {
-            console.log("uploadFile err", err)
-        }
-    }
+	async uploadFile(file: Express.Multer.File, key: string) {
+		console.log('inside upload file');
+		const bucket = this.configService.get<string>('S3_BUCKET');
+		const expiresIn = this.configService.get<number>('EXPIRES_IN');
+		const input: PutObjectCommandInput = {
+			Body: file.buffer,
+			Bucket: bucket,
+			Key: key,
+			ContentType: file.mimetype,
+		};
+		console.log('input', input);
+		try {
+			const response: PutObjectCommandOutput = await this.s3.send(
+				new PutObjectCommand(input),
+			);
+			console.log('response', response);
+			if (response.$metadata.httpStatusCode === 200) {
+				const client = this.s3;
+				const command = new GetObjectCommand({
+					Bucket: bucket,
+					Key: key,
+				});
+				return getSignedUrl(client, command, { expiresIn: expiresIn });
+			}
+			throw new Error('File not saved to s3!');
+		} catch (err) {
+			console.log('uploadFile err', err);
+		}
+	}
 
-    async getFileUrl(key: string) {
-        console.log("inside getFileUrl")
-        const bucket = this.configService.get<string>('S3_BUCKET')
-        const expiresIn = this.configService.get<number>('EXPIRES_IN')
-        
-        try {
-            const client = this.s3;
-            const command = new GetObjectCommand({ Bucket: bucket, Key: key });
-            return getSignedUrl(client, command, { expiresIn: expiresIn });
-            
-        } catch (err) {
-            console.log("getFileUrl err", err)
-        }
-    }
+	async getFileUrl(key: string) {
+		const bucket = this.configService.get<string>('S3_BUCKET');
+		const expiresIn = this.configService.get<number>('EXPIRES_IN');
+
+		try {
+			const client = this.s3;
+			const command = new GetObjectCommand({ Bucket: bucket, Key: key });
+			return getSignedUrl(client, command, { expiresIn: expiresIn });
+		} catch (err) {
+			console.log('getFileUrl err', err);
+		}
+	}
+
+	async deletePhoto(photoKey: string) {
+		try {
+			const params = {
+				Bucket: this.configService.get<string>('S3_BUCKET'),
+				Key: photoKey,
+			};
+			const client = this.s3;
+			const command = new DeleteObjectCommand(params);
+			return await client.send(command);
+		} catch (error) {
+			console.log('error occur', error);
+			throw new Error(error.message);
+		}
+	}
 }
--- src/src/upload-file/upload-file.service.ts
+++ src/src/upload-file/upload-file.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { Response } from 'express';
 import { HasuraService } from 'src/services/hasura/hasura.service';
 import { S3Service } from 'src/services/s3/s3.service';
+import { HasuraService as HasuraServiceFromServices } from '../services/hasura/hasura.service';
 
 @Injectable()
 export class UploadFileService {
 	constructor(
+		private hasuraServiceFromServices: HasuraServiceFromServices,
+
 		private readonly s3Service: S3Service,
 		private readonly hasuraService: HasuraService,
 	) {}
-
 	async addFile(
 		file: Express.Multer.File,
 		id: number,
@@ -23,26 +25,77 @@ export class UploadFileService {
 		const [name, fileType] = originalName.split('.');
 		let key = `${name}${Date.now()}.${fileType}`;
 		const fileUrl = await this.s3Service.uploadFile(file, key);
-		if (fileUrl) {
-			let query = {
-				query: `mutation MyMutation {
-                    insert_documents(objects: {name: "${key}", path: "/user/docs", provider: "s3", updated_by: "${id}", user_id: "${id}", doument_type: "${document_type}", document_sub_type: "${document_type}", created_by: "${id}"}) {
-                      affected_rows
-                      returning {
+		const documentTypeArray = ['aadhaar_front', 'aadhaar_back'];
+		if (documentTypeArray.includes(document_type)) {
+			try {
+				const data = {
+					query: `query MyQuery {
+                    users(where: {id: {_eq: ${id}}}) {
+                      id
+                      username
+                      mobile
+                      ${document_type}: documents(where: {document_sub_type: {_eq: "${document_type}"}}) {
                         id
+                        name
                         doument_type
                         document_sub_type
                         path
-                        name
-                        user_id
-                        updated_by
-                        provider
-                        created_by
-                        context_id
-                        context
                       }
                     }
                   }`,
+				};
+				//fetch documents data based on id and docuent_type
+				const response = await this.hasuraServiceFromServices.getData(
+					data,
+				);
+				let result = response?.data?.users;
+				let FileData: any = result[0][document_type];
+				if (FileData.length > 0) {
+					const promise = [];
+					const promise2 = [];
+					for (let item of FileData) {
+						//add all exist records into promise
+						promise.push(
+							this.hasuraService.delete('documents', {
+								id: item.id,
+							}),
+						);
+						promise2.push(this.s3Service.deletePhoto(item.name));
+					}
+					//delete all existing records from table
+					const deleteDocuments = await Promise.all(promise);
+					//delete all existing records from s3 bucket
+					const deleteFroms3Bucket = await Promise.all(promise2);
+				}
+			} catch (error) {
+				return response.status(500).send({
+					success: false,
+
+					message: 'Unable to Upload documents',
+					data: {},
+				});
+			}
+		}
+		if (fileUrl) {
+			let query = {
+				query: `mutation MyMutation {
+                  insert_documents(objects: {name: "${key}", path: "/user/docs", provider: "s3", updated_by: "${id}", user_id: "${id}", doument_type: "${document_type}", document_sub_type: "${document_type}", created_by: "${id}"}) {
+                    affected_rows
+                    returning {
+                      id
+                      doument_type
+                      document_sub_type
+                      path
+                      name
+                      user_id
+                      updated_by
+                      provider
+                      created_by
+                      context_id
+                      context
+                    }
+                  }
+                }`,
 			};
 			const res = await this.hasuraService.postData(query);
 
