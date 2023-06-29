c49b27ba2df73bd66e80b22891c291c2c6c63264
yogpawar1206 <107765181+yogpawar1206@users.noreply.github.com>
2023-06-29 22:11:36 +0530

    fomatted code

--- src/src/beneficiaries/beneficiaries.controller.ts
+++ src/src/beneficiaries/beneficiaries.controller.ts
@@ -92,12 +92,12 @@ export class BeneficiariesController {
 	@Put('statusUpdate')
 	@UseGuards(new AuthGuard())
 	@UsePipes(ValidationPipe)
-	async statusUpdate(@Body() request: StatusUpdateDTO,@Res() response: any) {
-	const result= await this.beneficiariesService.statusUpdate(request);
-	return response.status(result.status).json({
-		success: result.success,
-		message: result.message,
-		data: result.data
-	});
+	async statusUpdate(@Body() request: StatusUpdateDTO, @Res() response: any) {
+		const result = await this.beneficiariesService.statusUpdate(request);
+		return response.status(result.status).json({
+			success: result.success,
+			message: result.message,
+			data: result.data,
+		});
 	}
 }
