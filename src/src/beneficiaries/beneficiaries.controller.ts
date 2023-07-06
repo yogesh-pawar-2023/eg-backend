39ed69fa95ac39c4b22f115e886e9c5ed4079d83
yogesh-pawar-2023 <133862746+yogesh-pawar-2023@users.noreply.github.com>
2023-07-06 15:44:18 +0530

    added Ag list for IP dashboard (#178)
    * added Ag list for IP dashboard
    
    * resolved changes
    
    ---------
    
    Co-authored-by: yogpawar1206 <107765181+yogpawar1206@users.noreply.github.com>

--- src/src/beneficiaries/beneficiaries.controller.ts
+++ src/src/beneficiaries/beneficiaries.controller.ts
@@ -48,6 +48,17 @@ export class BeneficiariesController {
 	) {
 		return this.beneficiariesService.findAll(request, req, response);
 	}
+
+	@Post('/admin/list')
+	@UseGuards(new AuthGuard())
+	findAllAgForIp(
+		@Body() request: Record<string, any>,
+		@Req() req: any,
+		@Res() response: Response,
+	) {
+		return this.beneficiariesService.getList(request, req, response);
+	}
+
 	@Get('/getStatuswiseCount')
 	getStatuswiseCount(@Req() request: any, @Res() response: Response) {
 		return this.beneficiariesService.getStatuswiseCount(request, response);
