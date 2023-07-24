fb397edb264fa47daf15058a24098c85ded7b523
yogpawar1206 <107765181+yogpawar1206@users.noreply.github.com>
2023-07-24 12:11:59 +0530

    formatted code

--- src/src/comments/comments.service.ts
+++ src/src/comments/comments.service.ts
@@ -106,11 +106,11 @@ export class CommentsService {
           updated_by
           updated_datetime
           status
-		  user_details{
-			first_name
-			last_name
-			id  
-		  }
+          user_details{
+           first_name
+           last_name
+           id  
+          }
           }
       }`,
 		};
