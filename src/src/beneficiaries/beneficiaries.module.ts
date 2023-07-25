9b5104bdb089ee0e452c61e479ebbcbca20d3abf
parthgandhitekdi <133663771+parthgandhitekdi@users.noreply.github.com>
2023-07-25 12:48:27 +0530

    Task #201948 Remove duplicate status from system (#213)
    * Update app.module.ts
    
    * Issue #000 chore: Sentry Integration (#28)
    
    * Update .gitignore
    
    * Interview Fields Updated on interview table (#201) (#202)
    
    * Interview Fields Updated on interview table
    
    * Interview Fields Updated on interview table
    
    * Updated fileds changes
    
    Co-authored-by: Tusharmahajan12 <121154132+Tusharmahajan12@users.noreply.github.com>
    
    * Removed duplicate status from system
    
    ---------
    
    Co-authored-by: Manoj L <manojLondhe@users.noreply.github.com>
    Co-authored-by: Tusharmahajan12 <121154132+Tusharmahajan12@users.noreply.github.com>

--- src/src/beneficiaries/beneficiaries.module.ts
+++ src/src/beneficiaries/beneficiaries.module.ts
@@ -11,6 +11,7 @@ import { HttpModule } from '@nestjs/axios';
 import { S3Module } from 'src/services/s3/s3.module';
 import { HasuraModule } from '../hasura/hasura.module';
 import { HelperModule } from '../helper/helper.module';
+import { EnumModule } from '../enum/enum.module';
 import { HasuraModule as HasuraModuleFromServices } from '../services/hasura/hasura.module';
 import { KeycloakModule } from '../services/keycloak/keycloak.module';
 import { BeneficiariesService } from './beneficiaries.service';
@@ -25,6 +26,7 @@ import { AuthMiddleware } from '../common/middlewares/authmiddleware';
 		KeycloakModule,
 		HasuraModuleFromServices,
 		S3Module,
+		EnumModule
 	],
 	controllers: [BeneficiariesController],
 	providers: [BeneficiariesService],
